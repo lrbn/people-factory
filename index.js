a4763f693f6327a3234a7b3447ebcb860559fd3a
brandon <lrbn86@gmail.com>
2017-06-06 11:10:58 -0400

    Extract color div into outerHTML;9C

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // console.log("It worked");
 const personForm = document.querySelector('form#personForm');
+
+function renderColor(color) {
+  const colorDiv = document.createElement('div');
+  colorDiv.style.backgroundColor = color;
+  colorDiv.style.width = '100px';
+  colorDiv.style.height = '50px';
+  return colorDiv;
+}
+
 function handleSubmit(ev) {
   ev.preventDefault(); // The event continues to propagate as usual with only the exception that the event does nothing if no event listeners call stopPropagation(), which terminates propagation at once.
   // console.log('Submit!');
@@ -7,22 +16,19 @@ function handleSubmit(ev) {
   // Use the values from both inputs in the 'h1'.
   const details = document.querySelector('#details');
   const name = f.personName.value;
-  const favoriteColor = f.favoriteColor.value;
+  const favoriteColor = f.favoriteColor.value; // hex value
   const age = f.age.value;
   // Must put width and height (or element won't show)
-  const colorDiv = `<div style="background-color: ${favoriteColor}; width: 100px; height: 50px"></div>`;
-  // details.innerHTML = '<em>' + name + '<em>';
-  // details.innerHTML = `<em>${name}</em>`; // ${} javascript expression
-  // const em = document.createElement('em');
-  // em.textContent = name;
-  // details.appendChild(em);
+
   details.innerHTML = `
     <ul>
       <li>Name: ${name}</li>
-      <li>Favorite Color: ${colorDiv}</li>
+      <li>Favorite Color: ${renderColor(favoriteColor).outerHTML}</li>
       <li>Age: ${age}</li>
     </ul>
-  `; 
-  
+  `;  
 }
+// outerHTML includes opening and closing tags
+// innerHTML includes only the inside between the opening and closing tags
+
 personForm.addEventListener('submit', handleSubmit); // Do not put () after handleSubmit because it gets the return value
